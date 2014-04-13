ea1ad8e3aeff1ef6f95a9d3b73312410777d349a
Torben Rahbek Koch <torben@rahbekkoch.dk>
2014-04-13 22:25:36 +0200

    Updated incorrect GameBoard exercise files

--- ColorPuzzle/Exercises/Exercise5/GameBoard.fs
+++ ColorPuzzle/Exercises/Exercise5/GameBoard.fs
@@ -15,7 +15,16 @@ module GameBoard =
         color    : TileColor
         position : Position
     }
-        
+
+    let randomGenerator = System.Random()
+
+    let getRandom n =
+        randomGenerator.Next n
+
+    let createRandomColor numberOfColors =
+        let colorIndex = getRandom numberOfColors
+        TileColor colorIndex
+            
     type GameBoard = {
         sizeX         : int
         sizeY         : int  
@@ -28,24 +37,150 @@ module GameBoard =
         isFinished    : bool
     }
 
-    let createGameBoard sizeX sizeY numberOfColors =       
+    let calculateCurrentTileSet sizeX sizeY (tiles : Tile array array) : Set<Tile> = 
+        let westTile tile =
+            match tile.position.x, tile.position.y with
+            | (x, y) when x > 0 -> Some(tiles.[y].[x-1])
+            | _ -> None
+
+        let eastTile tile =
+            match tile.position.x, tile.position.y with
+            | (x, y) when x < sizeX-1 -> Some(tiles.[y].[x+1])
+            | _ -> None
+        
+        let northTile tile =
+            match tile.position.x, tile.position.y with
+            | (x, y) when y > 0 -> Some(tiles.[y-1].[x])
+            | _ -> None
+            
+        let southTile tile =
+            match tile.position.x, tile.position.y with
+            | (x, y) when y < sizeY-1 -> Some(tiles.[y+1].[x])
+            | _ -> None
+
+        let rec goWest color tile tiles =
+            let w = westTile tile
+            match w with
+            | Some(west) when west.color = color -> goWest color west (west::tiles)
+            | _ -> tiles
+
+        let rec goEast color tile tiles =
+            let e = eastTile tile
+            match e with
+            | Some(east) when east.color = color -> goEast color east (east::tiles)
+            | _ -> tiles
+
+        let q = Queue<Tile>()
+        let tileSet = List<Tile>()
+
+        let paintedTiles =         
+            [| for y in 0..sizeY-1 -> 
+                [|for x in 0..sizeX-1 -> 
+                    { x = x; y = y; painted=false} |]  |] 
+
+        let hasBeenPainted tile =
+            paintedTiles.[tile.position.y].[tile.position.x].painted
+
+        let paintTile tile =
+            paintedTiles.[tile.position.y].[tile.position.x] <- 
+                { x = tile.position.x; y = tile.position.y; painted = true }
+
+        q.Enqueue tiles.[0].[0]
+        let color = tiles.[0].[0].color
+
+        let rec workThroughQueue() =
+            match q.Count with
+            | 0 -> ()
+            | _ ->
+                let currentTile = q.Dequeue()
+                if currentTile.color = color && not (hasBeenPainted currentTile) then
+                    paintTile currentTile
+
+                    let line = List<Tile>()
+                    line.Add currentTile
+                    line.AddRange(goWest color currentTile [])
+                    line.AddRange(goEast color currentTile [])
+
+                    for tile in line do
+                        paintTile tile
+                        let n = northTile tile
+                        match n with
+                        | Some(n) when n.color = color -> q.Enqueue n
+                        | _ -> ()
+
+                        let s = southTile tile
+                        match s with
+                        | Some(s) when s.color = color -> q.Enqueue s
+                        | _ -> ()
+                workThroughQueue()
+
+        workThroughQueue()
+        paintedTiles
+        |> Array.collect (fun line -> line |> Array.filter (fun tile -> tile.painted))
+        |> Array.map(fun tile -> tiles.[tile.y].[tile.x])
+        |> Set.ofArray
+
+    let createTiles sizeX sizeY numberOfColors =
+        [| for y in 0..sizeY-1 -> 
+            [|for x in 0..sizeX-1 -> 
+                {color = createRandomColor numberOfColors; position = {x = x; y = y} }|]  |] 
+
+    let createGameBoard sizeX sizeY numberOfColors =
+        let tiles = createTiles sizeX sizeY numberOfColors
         { 
-            currentColor = TileColor(0)
+            currentColor = tiles.[0].[0].color
             sizeX        = sizeX
             sizeY        = sizeY 
             numberOfColors = numberOfColors
-            tiles        = Array.empty
-            currentTiles = Set []
+            tiles        = tiles
+            currentTiles = calculateCurrentTileSet sizeX sizeY tiles
             numberOfMoves = 0
             score         = 0
             isFinished    = false
         }
-
+            
     let moveBoardToNextLevel gameBoard = 
         gameBoard
 
     let paintBoardWithColor gameBoard newColor =
-        gameBoard                   
+        if gameBoard.isFinished then
+            gameBoard
+        elif gameBoard.currentColor = newColor then
+            gameBoard        
+        else
+            let sizeX = gameBoard.sizeX
+            let sizeY = gameBoard.sizeY
+
+            let tiles = [| for y in 0..sizeY-1 ->
+                            [|for x in 0..sizeX-1 ->
+                               let tile = gameBoard.tiles.[y].[x]
+                               if Set.contains tile gameBoard.currentTiles then
+                                   { tile with color = newColor }
+                               else
+                                   tile
+                            |]
+                        |]         
+
+            let newCurrentTiles = calculateCurrentTileSet sizeX sizeY tiles
+            let isFinished = newCurrentTiles.Count = gameBoard.sizeX * gameBoard.sizeY
+
+            if isFinished then
+                { gameBoard with 
+                    tiles = tiles
+                    isFinished = true
+                    numberOfMoves = gameBoard.numberOfMoves + 1
+                 }
+            else            
+                if newCurrentTiles.Count = gameBoard.currentTiles.Count then
+                    gameBoard
+                else
+                    { gameBoard with 
+                        currentColor = newColor
+                        score = newScore
+                        tiles = tiles 
+                        currentTiles = newCurrentTiles
+                        numberOfMoves = gameBoard.numberOfMoves + 1
+                     }
 
     module Persistence =
 
